07d788d281d0a2538aaed70f8efa5fbe7ac76c8f
Raul Mariscal Mariscal <121414814+mariscaldev@users.noreply.github.com>
2023-08-03 05:26:48 -0400

    primer push

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  scrollTo(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
